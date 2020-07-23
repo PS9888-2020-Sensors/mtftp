ecfbd3008b5d8534371e90c5b5b3af338fee8331
Justin Ong <justin.ong.97@gmail.com>
2020-07-23 09:34:58 +0800

    Fix onIdle not being called on read failure

--- mtftp_server.cpp
+++ mtftp_server.cpp
@@ -103,40 +103,58 @@ void MtftpServer::onPacketRecv(const uint8_t *data, uint16_t len_data) {
 }
 
 void MtftpServer::loop(void) {
-  if (state == STATE_TRANSFER) {
-    packet_data_t data_pkt;
+  enum server_state new_state = STATE_NOCHANGE;
 
-    data_pkt.block_no = transfer_params.block_no;
+  switch(state) {
+    case STATE_TRANSFER:
+    {
+      packet_data_t data_pkt;
 
-    uint16_t offset = transfer_params.file_offset + (transfer_params.block_no * CONFIG_LEN_BLOCK);
-    if (!readFile(
-      transfer_params.file_index,
-      offset,
-      data_pkt.block,
-      CONFIG_LEN_BLOCK,
-      &transfer_params.bytes_read
-    )) {
-      ESP_LOGW(TAG, "loop: reading from %d at offset %d failed. state=IDLE", transfer_params.file_index, offset);
-      packet_err_t err_pkt;
+      data_pkt.block_no = transfer_params.block_no;
 
-      err_pkt.err = ERR_FREAD;
+      uint16_t offset = transfer_params.file_offset + (transfer_params.block_no * CONFIG_LEN_BLOCK);
+      if (!readFile(
+        transfer_params.file_index,
+        offset,
+        data_pkt.block,
+        CONFIG_LEN_BLOCK,
+        &transfer_params.bytes_read
+      )) {
+        ESP_LOGW(TAG, "loop: reading from %d at offset %d failed. state=IDLE", transfer_params.file_index, offset);
+        packet_err_t err_pkt;
 
-      sendPacket((uint8_t *) &err_pkt, sizeof(err_pkt));
+        err_pkt.err = ERR_FREAD;
 
-      state = STATE_IDLE;
-      return;
+        sendPacket((uint8_t *) &err_pkt, sizeof(err_pkt));
+
+        new_state = STATE_IDLE;
+        break;
+      }
+
+      sendPacket((uint8_t *) &data_pkt, LEN_DATA_HEADER + transfer_params.bytes_read);
+
+      if (transfer_params.bytes_read < CONFIG_LEN_BLOCK) {
+        // just read final block available
+        new_state = STATE_WAIT_ACK;
+      } else if (transfer_params.block_no >= (transfer_params.window_size - 1)) {
+        // sent transfer_params.window_size blocks
+        new_state = STATE_WAIT_ACK;
+      } else {
+        transfer_params.block_no ++;
+      }
+      break;
+    }
+    default:
+      break;
+  }
+
+  if (new_state != STATE_NOCHANGE) {
+    ESP_LOGI(TAG, "loop: state change from %s to %s", server_state_str[state], server_state_str[new_state]);
+
+    if (new_state == STATE_IDLE) {
+      if (*onIdle != NULL) onIdle();
     }
 
-    sendPacket((uint8_t *) &data_pkt, LEN_DATA_HEADER + transfer_params.bytes_read);
-
-    if (transfer_params.bytes_read < CONFIG_LEN_BLOCK) {
-      // just read final block available
-      state = STATE_WAIT_ACK;
-    } else if (transfer_params.block_no >= (transfer_params.window_size - 1)) {
-      // sent transfer_params.window_size blocks
-      state = STATE_WAIT_ACK;
-    } else {
-      transfer_params.block_no ++;
-    }
+    state = new_state;
   }
 }
